f52486ccf71d7dd8c51abd1bf0a2f0e9d7cef274
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-10-22 00:44:42 +0300

    change font size and height

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -2,35 +2,89 @@
 
 var gCanvas;
 var gCtx;
+var currMeme = getMeme()
 
-function onInit(){
-    renderGallery()
+
+function onInit() {
     gCanvas = document.querySelector('#my-canvas');
     gCtx = gCanvas.getContext('2d');
-    console.log(gCtx)
-    
+    renderGallery()
+    renderMeme()
+
+}
+function renderMeme() {
+    var img = findImgById(currMeme.selectedImgId);
+    drawImg(img.url)
+}
+function onUpdateMeme(id) {
+    updateMeme(id);
+    renderMeme();
+}
+
+///draw img on canvas
+function drawImg(imgUrl) {
+    var img = new Image();
+    img.src = imgUrl
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        renderTxt(currMeme.lines);
+    };
+}
+
+///change text
+function changeText(value) {
+    console.log(value);
+    var currIdx = currMeme.selectedLineIdx;
+    changeLineTxt(value, currIdx);
+    renderMeme()
+}
+
+///render txt
+function renderTxt(txt) {
+    txt.forEach(line => {
+        drawText(line.txt, line.size,line.color,line.align , line.x, line.y, line.font)
+        console.log('txt', line.txt);
+        
+    });
 }
 
 
 
-function renderGallery(){
-    const elGallery= document.querySelector('.gallery-content')
-    const gallery= getGalleryData()
-    console.log(gallery);
+///text 
+function drawText(txt, size, color, align, x, y, font = 'IMPACT') {
+    gCtx.beginPath()
+    gCtx.strokeStyle = 'black';
+    gCtx.font = `${size}px ${font}`;
+    gCtx.lineWidth = '2';
+    gCtx.textAlign = align;
+    gCtx.fillStyle = color;
+    gCtx.fillText(txt, x, y);
+    gCtx.strokeText(txt, x, y);
+    // console.log('font',gCtx.font,'strokestyle', gCtx.strokeStyle , 'txt',gCtx.fillText(txt,x,y));
     
-    const strDivs = gallery.map( img=> {
-        return `<div class="picture"  onclick="drawImg(${img.id})">
-                    <img id="${img.id}" src=${img.url} alt=${img.id}>
-                </div>`;
-    })
+}
+//font size
+function onChangeFontSize(num){
+    changeFontSize(num);
+    renderMeme();
+}
+//line height
+function onChangeHeightLine(num){
+    changeHieghtLine(num);
+    renderMeme();
+}
+
+
+
+
+function renderGallery() {
+    const elGallery = document.querySelector('.gallery-content')
+    const gallery = getGalleryData()
+
+    const strDivs = gallery.map(img => {
+        return `<div class="picture"  onclick="onUpdateMeme(${img.id})">
+     <img id="${img.id}" src=${img.url} alt=${img.id}>
+      </div>`;
+    });
     elGallery.innerHTML = strDivs.join('');
-}
-
-
-function drawText() { 
-    var text = userTextInput();
-    gCtx.font = `24px IMPACT`
-    gCtx.textAlign = 'start'
-    gCtx.fillText(text,100 ,100)
-    gCtx.strokeText(text, 100, 100)
 }
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,79 +1,94 @@
 'use strict';
-var gCanvas;
-var gCtx;
 
 
 //gImgs
-const gImgs =[
-    {id:1, url: 'meme-imgs/1.jpg', keywords:[]},
-    {id:2, url: 'meme-imgs/2.jpg', keywords:[]},
-    {id:3, url: 'meme-imgs/3.jpg', keywords:[]},
-    {id:4, url: 'meme-imgs/4.jpg', keywords:[]},
-    {id:5, url: 'meme-imgs/5.jpg', keywords:[]},
-    {id:6, url: 'meme-imgs/6.jpg', keywords:[]},
-    {id:7, url: 'meme-imgs/7.jpg', keywords:[]},
-    {id:8, url: 'meme-imgs/8.jpg', keywords:[]},
-    {id:9, url: 'meme-imgs/9.jpg', keywords:[]},
-    {id:10, url: 'meme-imgs/10.jpg', keywords:[]},
-    {id:11, url: 'meme-imgs/11.jpg', keywords:[]},
-    {id:12, url: 'meme-imgs/12.jpg', keywords:[]},
-    {id:13, url: 'meme-imgs/13.jpg', keywords:[]},
-    {id:14, url: 'meme-imgs/14.jpg', keywords:[]},
-    {id:15, url: 'meme-imgs/15.jpg', keywords:[]},
-    {id:16, url: 'meme-imgs/16.jpg', keywords:[]},
-    {id:17, url: 'meme-imgs/17.jpg', keywords:[]},
-    {id:18, url: 'meme-imgs/18.jpg', keywords:[]}
-] 
-/// gmeme
+const gImgs = [
+    { id: 1, url: 'meme-imgs/1.jpg', keywords: [] },
+    { id: 2, url: 'meme-imgs/2.jpg', keywords: [] },
+    { id: 3, url: 'meme-imgs/3.jpg', keywords: [] },
+    { id: 4, url: 'meme-imgs/4.jpg', keywords: [] },
+    { id: 5, url: 'meme-imgs/5.jpg', keywords: [] },
+    { id: 6, url: 'meme-imgs/6.jpg', keywords: [] },
+    { id: 7, url: 'meme-imgs/7.jpg', keywords: [] },
+    { id: 8, url: 'meme-imgs/8.jpg', keywords: [] },
+    { id: 9, url: 'meme-imgs/9.jpg', keywords: [] },
+    { id: 10, url: 'meme-imgs/10.jpg', keywords: [] },
+    { id: 11, url: 'meme-imgs/11.jpg', keywords: [] },
+    { id: 12, url: 'meme-imgs/12.jpg', keywords: [] },
+    { id: 13, url: 'meme-imgs/13.jpg', keywords: [] },
+    { id: 14, url: 'meme-imgs/14.jpg', keywords: [] },
+    { id: 15, url: 'meme-imgs/15.jpg', keywords: [] },
+    { id: 16, url: 'meme-imgs/16.jpg', keywords: [] },
+    { id: 17, url: 'meme-imgs/17.jpg', keywords: [] },
+    { id: 18, url: 'meme-imgs/18.jpg', keywords: [] }
+]
+/// gMeme
 var gMeme = {
-    selectedImgId: null,
+    selectedImgId: 1,
     selectedLineIdx: 0,
-
     lines: [
         {
             txt: 'I never eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red'
+            size: 28,
+            align: 'center',
+            color: 'white',
+            font: 'IMPACT',
+            x: 200,
+            y: 50
+        },
+        {
+            txt: 'who likes Falafel',
+            size: 28,
+            align: 'center',
+            color: 'white',
+            font: 'IMPACT',
+            x: 200,
+            y: 340
         }
     ]
 }
-///get gallery data
-function getGalleryData() {
-    let imgs = gImgs
-    console.log(imgs);
-    
-    return imgs
+///get meme
+function getMeme() {
+    return gMeme;
 }
-
 
 
 //find img by id
 function findImgById(imgId) {
-    return gImgs.find(img=>{
+    return gImgs.find(img => {
         return img.id === imgId
     });
 }
-function userTextInput(){
-    var userInput = { txt: document.getElementById('text').value,
-    size: 20,
-    align: 'left',
-    color: 'red'
-};
- gMeme.lines.push(userInput);
- return userInput.txt;
+
+
+function changeLineTxt(txt, lineIdx) {
+    return gMeme.lines[lineIdx].txt = txt
 }
 
-///get meme
-function getCurrMeme() {
-    return gMeme;
+
+
+function updateMeme(id) {
+    gMeme.selectedImgId = id;
 }
-///draw img on canvas
-function drawImg(imgId) {
-    var img = new Image()
-    let currImg = findImgById(imgId)
-    img.src = currImg.url
-    img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-    }
+hieght
+
+
+//change font size
+function changeFontSize(num) {
+    if (gMeme.lines[0].size === 100 || gMeme.lines[0].size === 5) return
+    gMeme.lines[0].size += num;
+    console.log('font size', gMeme.lines[0].size);
+}
+function changeHieghtLine(num){
+    if(gMeme.lines[0].y === 0 || gMeme.lines[0].y === 350) return
+    gMeme.lines[0].y += num;
+    console.log('font size', gMeme.lines[0].y)
+
+}
+
+
+///get gallery data
+function getGalleryData() {
+    let imgs = gImgs
+    return imgs
 }
