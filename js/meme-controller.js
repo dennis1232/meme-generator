4e1f0d5a13dc81e1ea7a1ee95529f06a19a50696
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-10-24 21:39:25 +0300

    final delivery

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -173,7 +173,6 @@ function downloadImg(elLink) {
     elLink.href = imgContent
 }
 
-// The next 2 functions handle IMAGE UPLOADING to img tag from file system: 
 function onImgInput(ev) {
     loadImageFromInput(ev, renderMeme)
 }
